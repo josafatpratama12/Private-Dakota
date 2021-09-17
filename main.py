d0305dfc4ebd89ba2afd1494b44b5ffff8c0b9f9
Josafat Pratama Susilo <53671663+josafatpratama12@users.noreply.github.com>
2021-09-17 03:46:11 +0000

    (+) Add handle command feature (+) Add run function

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("test")
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(msg):
+  if msg.author == client.user:
+    return
+  
+  if msg.content.startswith('$hello'):
+    await msg.channel.send('Hello!')
+  
+client.run(os.getenv("DISCORD_BOT_TOKEN"))
