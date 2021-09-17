eabe14283c8a294e0cc2ddae70cb56df66c3fc86
Josafat Pratama Susilo <53671663+josafatpratama12@users.noreply.github.com>
2021-09-17 04:45:11 +0000

    (+) Add Motivational Quotes Feature

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import discord
 import os
+import requests
+import json
 
 client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = '"' + json_data[0]['q'] + '" -' + json_data[0]['a']
+  return(quote)
 
 @client.event
 async def on_ready():
@@ -18,5 +26,9 @@ async def on_message(msg):
   if msg.content.startswith('$bilang'):
     msg_content = msg.content
     await msg.channel.send(msg.content.replace("$bilang",""))
+
+  if msg.content.startswith('$motivasi'):
+    quote = get_quote()
+    await msg.channel.send(quote)
   
 client.run(os.getenv("DISCORD_BOT_TOKEN"))
